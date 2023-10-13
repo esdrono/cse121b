644b78d08dbae44bd828360ffcdfde7a4a28d37c
esdrono <77854186+esdrono@users.noreply.github.com>
2023-10-13 10:39:45 -0600

    Add files via upload

---
+++ w05-task/w05-task.js
@@ -0,0 +1,56 @@
+// Declare Global Variables
+const templesElement = document.getElementById('temples');
+let templeList = [];
+
+// Function: displayTemples()
+const displayTemples = (temples) => {
+    temples.forEach(temple => {
+        const article = document.createElement('article');
+        const h3 = document.createElement('h3');
+        h3.textContent = temple.templeName;
+        const img = document.createElement('img');
+        img.src = temple.imageUrl;
+        img.alt = temple.location;
+        article.appendChild(h3);
+        article.appendChild(img);
+        templesElement.appendChild(article);
+    });
+};
+
+// Function: getTemples()
+const getTemples = async () => {
+    const response = await fetch("https://byui-cse.github.io/cse121b-ww-course/resources/temples.json");
+    templeList = await response.json();
+    displayTemples(templeList);
+};
+
+// Function: reset()
+const reset = () => {
+    templesElement.innerHTML = '';
+};
+
+// Function: sortBy()
+const sortBy = (temples) => {
+    reset();
+    const filter = document.querySelector("#sortBy").value;
+    switch (filter) {
+        case "utah":
+            displayTemples(temples.filter(temple => temple.location.includes("Utah")));
+            break;
+        case "nonutah":
+            displayTemples(temples.filter(temple => !temple.location.includes("Utah")));
+            break;
+        case "older":
+            displayTemples(temples.filter(temple => new Date(temple.dedicated) < new Date(1950, 0, 1)));
+            break;
+        case "all":
+            displayTemples(temples);
+            break;
+    }
+};
+
+// Event Listener: sortBy Element change
+document.querySelector("#sortBy").addEventListener("change", () => { sortBy(templeList) });
+
+// Call getTemples() to test your code
+getTemples();
